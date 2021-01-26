f54b678d7919955f881c8c9db077a7e0ee21373d
Vivek Jain <vkjvivek7@gmail.com>
2021-01-27 00:18:51 +0530

    setup rtk

--- app/configureStore.ts
+++ app/configureStore.ts
@@ -2,12 +2,15 @@
  * Create the store with dynamic reducers
  */
 
-import { applyMiddleware, createStore, compose } from 'redux';
+import { applyMiddleware } from 'redux';
+// import { applyMiddleware, compose } from 'redux';
 import { routerMiddleware } from 'connected-react-router';
+import { configureStore as reduxToolKitconfigureStore } from '@reduxjs/toolkit';
+
 import { createInjectorsEnhancer, forceReducerReload } from 'redux-injectors';
 import createSagaMiddleware from 'redux-saga';
 import { History } from 'history';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 import createReducer from './reducers';
 import { InjectedStore, ApplicationRootState } from 'types';
@@ -33,26 +36,32 @@ export default function configureStore(
     }),
   ];
 
-  let enhancer;
-  // If Redux Dev Tools and Saga Dev Tools Extensions are installed, enable them
-  /* istanbul ignore next */
-  if (process.env.NODE_ENV !== 'production' && typeof window === 'object') {
-    enhancer = composeWithDevTools(...enhancers);
-    // NOTE: Uncomment the code below to restore support for Redux Saga
-    // Dev Tools once it supports redux-saga version 1.x.x
-    // if (window.__SAGA_MONITOR_EXTENSION__)
-    //   reduxSagaMonitorOptions = {
-    //     sagaMonitor: window.__SAGA_MONITOR_EXTENSION__,
-    //   };
-  } else {
-    enhancer = compose(...enhancers);
-  }
+  const createEnhancers = () => {
+    let enhancer;
+    // If Redux Dev Tools and Saga Dev Tools Extensions are installed, enable them
+    /* istanbul ignore next */
+    if (process.env.NODE_ENV !== 'production' && typeof window === 'object') {
+      // console.log(composeWithDevTools(...enhancers));
+      // enhancer = composeWithDevTools(...enhancers);
+      enhancer = enhancers;
+      // NOTE: Uncomment the code below to restore support for Redux Saga
+      // Dev Tools once it supports redux-saga version 1.x.x
+      // if (window.__SAGA_MONITOR_EXTENSION__)
+      //   reduxSagaMonitorOptions = {
+      //     sagaMonitor: window.__SAGA_MONITOR_EXTENSION__,
+      //   };
+    } else {
+      // console.log(composeWithDevTools(...enhancers));
+      enhancer = enhancers;
+    }
+    return enhancer;
+  };
 
-  const store = createStore(
-    createReducer(),
-    initialState,
-    enhancer,
-  ) as InjectedStore;
+  const store = reduxToolKitconfigureStore({
+    reducer: createReducer(),
+    enhancers: createEnhancers(),
+    preloadedState: initialState,
+  }) as InjectedStore;
 
   // Extensions
   store.runSaga = sagaMiddleware.run;
