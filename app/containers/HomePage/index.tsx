51713a775e63741f5fcf71aa3c8f714dc74b22c2
Vivek Jain <vkjvivek7@gmail.com>
2021-02-03 10:43:10 +0530

    Login form unfinished

--- app/containers/HomePage/index.tsx
+++ app/containers/HomePage/index.tsx
@@ -4,162 +4,62 @@
  * This is the first thing users see of our App, at the '/' route
  */
 
-import React, { useEffect } from 'react';
-import { Helmet } from 'react-helmet-async';
-import { FormattedMessage } from 'react-intl';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import {
-  Button,
-  Checkbox,
-  FormControlLabel,
-  FormGroup,
-  Grid,
-  Paper,
-  TextField,
-} from '@material-ui/core';
-
-// import { useInjectReducer } from 'utils/injectReducer';
-// import { useInjectSaga } from 'utils/injectSaga';
 import { useInjectReducer, useInjectSaga } from 'utils/redux-injectors';
-
-import {
-  makeSelectError,
-  makeSelectLoading,
-  makeSelectRepos,
-} from 'containers/App/selectors';
-import H2 from 'components/H2';
-import ReposList from 'components/ReposList';
-import AtPrefix from './AtPrefix';
-import CenteredSection from './CenteredSection';
-import Form from './Form';
-import Input from './Input';
-import Section from './Section';
-import messages from './messages';
-import { loadRepos } from '../App/actions';
-import { changeUsername } from './actions';
-import { loginLoading } from './reducer';
-import { makeSelectUsername } from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 
 const key = 'home';
 
-// const stateSelector = createStructuredSelector({
-//   repos: makeSelectRepos(),
-//   username: makeSelectUsername(),
-//   loading: makeSelectLoading(),
-//   error: makeSelectError(),
-// });
-
 const stateSelector = ({ home }) => ({
   home: home ?? {},
 });
 
-export default function HomePage() {
+const Home = () => {
+  useInjectReducer({ key: key, reducer: reducer });
+  useInjectSaga({ key: key, saga: saga });
+  const { handleSubmit, register, errors } = useForm();
+  const onSubmit = values => console.log(values);
   const {
     home: { login },
   } = useSelector(stateSelector);
-  console.log({ login });
 
   const dispatch = useDispatch();
 
-  // Not gonna declare event types here. No need. any is fine
-  // const onChangeUsername = (evt: any) =>
-  //   dispatch(changeUsername(evt.target.value));
-  // const onSubmitForm = (evt?: any) => {
-  //   if (evt !== undefined && evt.preventDefault) {
-  //     evt.preventDefault();
-  //   }
-  //   if (!username) {
-  //     return;
-  //   }
-  //   dispatch(loadRepos());
-  // };
-
-  useInjectReducer({ key: key, reducer: reducer });
-  useInjectSaga({ key: key, saga: saga });
-
-  // useEffect(() => {
-  //   // When initial state username is not null, submit the form to load repos
-  //   if (username && username.trim().length > 0) {
-  //     onSubmitForm();
-  //   }
-  // }, []);
-
-  // const reposListProps = {
-  //   loading: loading,
-  //   error: error,
-  //   repos: repos,
-  // };
-
   return (
-    <article>
-      <Helmet>
-        <title>Home Page</title>
-        <meta
-          name="description"
-          content="A React.js Boilerplate application homepage"
-        />
-      </Helmet>
-      <div style={{ width: '50%', textAlign: 'center' }}>
-        {' '}
-        <Paper elevation="3">
-          <div>
-            <Grid container>
-              <Grid item md sm xs>
-                <TextField
-                  id="username"
-                  label="Username"
-                  type="email"
-                  fullWidth
-                  autoFocus
-                  required
-                />
-              </Grid>
-            </Grid>
-            <Grid container>
-              <Grid item md sm xs>
-                <TextField
-                  id="username"
-                  label="Password"
-                  type="password"
-                  fullWidth
-                  required
-                />
-              </Grid>
-            </Grid>
-            <Grid container alignItems="center" justify="space-between">
-              <Grid item>
-                <FormControlLabel
-                  control={<Checkbox color="primary" />}
-                  label="Remember me"
-                />
-              </Grid>
-              <Grid item>
-                <Button
-                  disableFocusRipple
-                  disableRipple
-                  style={{ textTransform: 'none' }}
-                  variant="text"
-                  color="primary"
-                >
-                  Forgot password ?
-                </Button>
-              </Grid>
-            </Grid>
-            <Grid container justify="center" style={{ marginTop: '10px' }}>
-              <Button
-                variant="outlined"
-                color="primary"
-                style={{ textTransform: 'none' }}
-              >
-                Login
-              </Button>
-            </Grid>
-          </div>
-        </Paper>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="row">
+        <div className="col-12">
+          <input
+            name="email"
+            ref={register({
+              required: 'Required',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'invalid email address',
+              },
+            })}
+          />
+          <br />
+          {errors.email && errors.email.message}
+        </div>
+        <div className="col-12">
+          <input
+            name="password"
+            type="password"
+            ref={register({
+              minLength: 8,
+            })}
+          />
+        </div>
+        <br />
+        {errors.password && 'Your Password should be greater than 8 letters'}
       </div>
-    </article>
+
+      <button type="submit">Submit</button>
+    </form>
   );
-}
+};
+export default Home;
